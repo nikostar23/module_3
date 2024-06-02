e54029481b13458098e46c65c9396a5f21bd3077
Nikolay <komissariat@mail.ru>
2024-06-02 23:51:34 +0300

    task 1 is correct

--- lesson_1/solution_1.py
+++ lesson_1/solution_1.py
@@ -19,27 +19,29 @@ class Library:
         self.books = [book for book in self.books if book.title != title]
         return self
 
-    def __getitem__(self, title):
-        for book in self.books:
-            if book.title == title:
-                return book
-        raise KeyError(f'Book with title {title} not found')
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            if index < 0 or index >= len(self.books):
+                raise IndexError("Index out of range")
+            return self.books[index]
+        elif isinstance(index, slice):
+            return self.books[index.start:index.stop:index.step]
+        elif isinstance(index, str):
+            index = next((i for i, book in enumerate(self.books) if book.title == index), None)
+            if index is None:
+                raise KeyError("Book not found")
+            return self.books[index]
+        else:
+            raise TypeError("Index must be an integer, a slice, or a string")
     
     def __contains__(self, title):
         return any(book.title == title for book in self.books)
 
-    def add_book(self, title, author):
-        new_book = Book(title, author)
-        self.books.append(new_book)
-        return self
-
-    def remove_book(self, title):
-        self.books = [book for book in self.books if book.title != title]
-        return self
-
 # Пример использования
 library = Library()
+
 library.add_book("Война и мир", "Лев Толстой").add_book("Преступление и наказание", "Федор Достоевский")
+library.add_book("Детство", "Лев Толстой").add_book("Братья Карамазовы", "Федор Достоевский")
 print(library.books)
 
 library.remove_book("Преступление и наказание")
@@ -50,3 +52,7 @@ print(library['Война и мир'])
 print("Преступление и наказание" in library)
 
 print("Война и мир" in library)
+
+print(library[0])
+
+print(library[1:])
