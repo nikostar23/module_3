5803f30b5cac0a15de89e6aec5b097397223fe97
Nikolay <komissariat@mail.ru>
2024-07-21 18:21:04 +0300

    task 4 is copleted

--- lesson_4/solution_2.py
+++ lesson_4/solution_2.py
@@ -1,24 +1,36 @@
 class FileHandler:
-    def read():
+    def read(self):
         pass
     def write(data: str):
         pass
-
-    @classmethod
-    def save_data(handler: FileHandler, data: str):
-        
 
 class TextFileHandler(FileHandler):
-    def read():
-        print("Файл был прочитан")
+    def __init__(self, filename):
+        self.filename = filename
     
-    def write(data: str):
-        print("Файл был записан")
+    def read(self):
+        print(f"Текстовый файл {self.filename} был прочитан.")
+    
+    def write(self, data: str):
+        print(f"Данные \"{data}\" были записаны в текстовый файл {self.filename}.")
 
 class BinaryFileHandler(FileHandler):
-    def read():
-        print("Файл был прочитан")
+    def __init__(self, filename):
+        self.filename = filename
     
-    def write(data: str):
-        print("Файл был записан")
+    def read(self):
+        print(f"Бинарный файл {self.filename} был прочитан.")
+    
+    def write(self, data: str):
+        print(f"Данные \"{data}\" были записаны в бинарный файл {self.filename}.")
 
+def save_data(handler: FileHandler, data: str):
+    handler.read()
+    handler.write(data)
+
+# Пример использования
+text_handler = TextFileHandler("example.txt")
+binary_handler = BinaryFileHandler("example.bin")
+
+save_data(text_handler, "Hello, World!")
+save_data(binary_handler, "0101010101")
